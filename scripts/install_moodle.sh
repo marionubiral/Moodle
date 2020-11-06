acccf8791b239be916a7f1f8c9011e7259acad1c
Mario Omogrosso <71182483+marionubiral@users.noreply.github.com>
2020-11-06 17:57:57 -0300

    Update install_moodle.sh

--- scripts/install_moodle.sh
+++ scripts/install_moodle.sh
@@ -242,8 +242,7 @@ set -ex
         # install Office 365 plugins
         curl -k --max-redirs 10 https://raw.githubusercontent.com/marionubiral/Moodle/master/local_o365_moodle38_2020020306.zip -L -o o365.zip
         unzip -q o365.zip
-	cp -r o365/* /moodle/html/moodle
-        rm -rf o365
+	cp -r o365 /moodle/html/moodle
 
     fi
 
